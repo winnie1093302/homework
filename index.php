8d3b93273fbec66e82e32f7284807826cdc4822b
winnie1093302 <100466495+winnie1093302@users.noreply.github.com>
2022-02-27 16:22:26 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+    <?php
+    $i=10;
+    $j=20;
+    echo "<b>Hello PHP!</b>";
+    echo "<br/>";
+    echo $i+$j;
+    echo "<br/>";
+    ?>
+
+
+
+    </body>
+</html>
