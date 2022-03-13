d327dfdde8b623e042635d310e06cc10ce56c772
winnie1093302 <100466495+winnie1093302@users.noreply.github.com>
2022-03-13 22:14:26 +0800

    Add files via upload

---
+++ 表單.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<title>
+    表單
+</title>
+<link rel="shortcut icon" href="/homework1/picture/墾丁.jpg" type="image/x-icon" />
+</head>
+<body>
+<h1>墾丁三日遊</h1>
+<body background="/homework1/picture/背景3.jpg" >
+<h2>1.行程內容</h2>
+<p><h3>想體驗南台灣最熱情的景點，好好放鬆一下嗎?</br>這次行程要帶大家用三天兩夜玩遍屏東，更要讓大家對屏東徹底改觀。</br>除了墾丁外，還有很多地方、許多景點更值得一遊！</br>跟著我們的步伐，讓你的屏東行程直接顛覆親朋好友的想像。
+</h3></p>
+<a href="https://bunnyann.com/kenting-three-days/" target="_blank">
+    <img src="picture/s1.jpg" width="200" height="200">
+</a>
+<a href="https://www.klook.com/zh-TW/blog/trip-pingtung-taiwan/" target="_blank">
+    <img src="picture/s2.jpg" width="200" height="200">
+</a>
+<h2>2.資料填寫</h2>
+<form action="answer.php" method="post">
+     <h3>Name: <input type="text" name="Name" required><br/>
+<br/>E-mail:<input type="email" name="mail" required><br/>
+<br/>Telephone:<input type="tel" name="phonenumber" placeholder="XXXX-XXX-XXX" required><br/>
+<br/>Gender:<input type="radio" name="gender">Male<input type="radio" name="gender">Female<br/>
+<br/>Food Preference:<input type="checkbox" name="meal">美式<input type="checkbox" name="meal">日式<input type="checkbox" name="meal">台式<input type="checkbox" name="meal">泰式<br/>
+<br/>T-shirt Size: 
+<select name="size">
+<option>S</option>
+<option>M</option>
+<option>L</option>
+<option>XL</option>
+<option>XXL</option>
+</select>
+<br/>
+<br/>Your Birthday: <input type="date" name="birthday"><br/>
+<br/>Tickets number: <input type="number" name="ticket"></h3>
+
+<br/><input type="submit">
+</form>
+<h2>T-shirt 尺寸對照表(男)</h2>
+<table border>
+<tr><th> </th><th>S</th><th>M</th><th>L</th><th>XL</th><th>XXL</th></tr>  
+<tr><th>身高/cm</th><td>162-168</td><td>167-173</td><td>172-178</td><td>177-183</td><td>182-188</td></tr>  
+<tr><th>胸圍/cm</th><td>87-93</td><td>91-97</td><td>95-101</td><td>99-105</td><td>103-109</td></tr>
+<tr><th>腰圍/cm</th><td>74-80</td><td>78-84</td><td>82-88</td><td>86-92</td><td>90-96</td></tr>
+</table>
+<h2>T-shirt 尺寸對照表(女)</h2>
+<table border>
+<tr><th> </th><th>S</th><th>M</th><th>L</th><th>XL</th><th>XXL</th></tr>  
+<tr><th>身高/cm</th><td>155-161</td><td>160-166</td><td>165-171</td><td>170-176</td><td>175-181</td></tr>  
+<tr><th>胸圍/cm</th><td>77-83</td><td>81-87</td><td>85-91</td><td>89-95</td><td>92-100</td></tr>
+<tr><th>腰圍/cm</th><td>63-69</td><td>67-73</td><td>71-77</td><td>74-82</td><td>78-86</td></tr>
+</table>
+
+</body>
+</html> 
